007805985c7e1bd1080552a4129bb02ab4f7855b
Alexander Logunov <unlovedru@gmail.com>
2022-06-15 22:12:02 +0300

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,5 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   
-  def self.sort_by_category(category)
-    Test.where(category_id: category).order(title: :desc)
-  end
+  scope :sort_by_category, ->(cetagory) {where(category_id: category_id).order(title: :desc)}
 end
