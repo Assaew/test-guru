bc9fe7f26d9a6ae8d8b88dfaad03e7c56d211682
assa555@mai.ru <80923241023assa>
2022-06-17 16:35:30 +0300

    lesson_3_part_4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ApplicationRecord
   has_many :completed_tests
   has_many :tests, through: :completed_tests
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :name, :email, :password, presence: true
 
   def test_by_level(level)
     tests.where(level: level)
